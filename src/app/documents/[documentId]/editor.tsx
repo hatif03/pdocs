44dbb406734ed9266ef052dee07cffc8e732c9e2
Mohammad Hatif Osmani <66199079+hatif03@users.noreply.github.com>
2025-02-21 14:33:04 +0530

    adding text color and highlight button features

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -12,6 +12,9 @@ import Image from '@tiptap/extension-image'
 import ImageResize from 'tiptap-extension-resize-image'
 import Underline from '@tiptap/extension-underline'
 import FontFamily from '@tiptap/extension-font-family'
+import TextStyle from '@tiptap/extension-text-style'
+import { Color } from '@tiptap/extension-color'
+import Highlight from '@tiptap/extension-highlight'
 
 import { useEditorStore } from '@/store/use-editor-store'
 import { set } from 'date-fns'
@@ -67,6 +70,11 @@ export const Editor = () => {
         ImageResize,  
         Underline,
         FontFamily,
+        TextStyle,
+        Color,
+        Highlight.configure({
+            multicolor: true,
+        })
     ],
     content: `
         <table>
