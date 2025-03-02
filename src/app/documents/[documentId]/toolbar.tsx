2af9a9b2abd803f5e87b07b2e0604411e328e7e3
Mohammad Hatif Osmani <66199079+hatif03@users.noreply.github.com>
2025-03-02 16:14:42 +0530

    list function added

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -25,7 +25,9 @@ import {
     AlignLeftIcon,
     AlignCenterIcon,
     AlignRightIcon,
-    AlignJustifyIcon
+    AlignJustifyIcon,
+    ListIcon,
+    ListOrderedIcon
 } from "lucide-react";
 
 import { 
@@ -47,7 +49,52 @@ import {
  import { type ColorResult, SketchPicker } from "react-color";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import TextAlign from "@tiptap/extension-text-align";
+import { on } from "events";
+
+
+const ListButton = () => {
+    const { editor } = useEditorStore();
+
+    const lists = [
+        {
+            label: "Bullet list",
+            icon: ListIcon,
+            isActive: () => editor?.isActive("bulletList"),
+            onClick: () => editor?.chain().focus().toggleBulletList().run(),
+        },
+        {
+            label: "Ordered list",
+            icon: ListOrderedIcon,
+            isActive: () => editor?.isActive("orderedList"),
+            onClick: () => editor?.chain().focus().toggleOrderedList().run(),
+        }
+    ]
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <button className="h-7 min-w-7 shrink-0 flex flex-col items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm">
+                    <ListIcon className=" size-4"/>
+                </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="p-1 flex flex-col gap-y-1">
+                {lists.map(({ label, icon: Icon, onClick, isActive }) => (
+                    <button
+                        key={label}
+                        onClick={onClick}
+                        className={cn(
+                            "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
+                            isActive() && "bg-neutral-200/80"
+                        )}
+                    >
+                        <Icon className=" size-4"/>
+                        <span className=" text-sm">{label}</span>
+                    </button>
+                ))}
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
 
 
 const AlignButton = () => {
@@ -465,7 +512,7 @@ export const Toolbar = () => {
         <ImageButton/>
         <AlignButton/>
         {/* TODO: Line height */}
-        {/* TODO: List */}
+        <ListButton/>
         {sections[2].map((item) => (
         <ToolbarButton key={item.label} {...item} />
         ))}
